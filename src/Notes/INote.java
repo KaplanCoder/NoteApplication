5a783fb8f3cb051f0d7cdc95078805468136ca8a
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-10 16:12:23 +0300

    INote interface, note and note groups classes created

---
+++ src/Notes/INote.java
@@ -0,0 +1,25 @@
+package src.Notes;
+//Component
+public interface INote {
+
+    /**
+     * Method that returns whether the object is Note or not.
+     * @return boolean
+     */
+    public boolean isNote();
+
+
+    /**
+     * Method that returns header text of object.
+     * @return String
+     */
+    public String header();
+
+    public int getId();
+    public String getTitle();
+    public String getType();
+    public void setParent(NoteGroup parent);
+    public NoteGroup getParent();
+
+ 
+}
